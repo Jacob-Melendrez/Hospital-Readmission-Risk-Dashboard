44b232927cf8e30002dc3bac857113fd55d56ff6
Jacob Melendrez <jacob.n.melendrez@gmail.com>
2026-04-21 17:11:14 -0400

    Updated comments in import_and_cleaning.py

--- notebooks/import_and_cleaning.py
+++ notebooks/import_and_cleaning.py
@@ -1,14 +1,26 @@
+# Library dealing with files and paths. 
 import os
+# Library for data manipulation and analysis. 
 import pandas as pd
 
+# Creates a variable and finds the base directory or folder our .py script is inside. 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# Creates a variable for the file path to the specific csv we are analyzing. 
 file_path = os.path.join(BASE_DIR, "..", "data", "diabetic_data.csv")
 
+# Prints out the script file we are working with. import_and_cleaning.py
+print("\n")
 print("Script file:", __file__)
+# Prints out the file path to the specific csv file we are analyzing. 
+print()
 print("Resolved CSV path:", file_path)
+print()
 
+#Assigns the dataframe to the csv file being read. 
 df = pd.read_csv(file_path)
 
+#This line searches through the entire dataframe for any cells containing the string "?" and replaces them with pd.NA (pandas' standard way to represent missing values). 
+# The modified dataframe is then reassigned back to df, so all subsequent operations work with the cleaned data.
 df = df.replace("?", pd.NA)
 
 df["is_readmitted_30"] = df["readmitted"].apply(lambda x: 1 if x == "<30" else 0)
